b0095835d63c45925b59a34de61ba3b9791d6521
Artyom <artemtiunelis@gmail.com>
2025-02-02 15:54:12 +0000

    fixed space tap

--- keyboards/keychron/k7_max/ansi/rgb/keymaps/artylemon/keymap.c
+++ keyboards/keychron/k7_max/ansi/rgb/keymaps/artylemon/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T, KC_Y, KC_U,   KC_I,     KC_O,    KC_P,      KC_LBRC, KC_RBRC,   KC_BSLS,          KC_HOME,
      KC_CAPS,  HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G, KC_H, HOME_J, HOME_K,   HOME_L,  HOME_SCLN, KC_QUOT,            KC_ENT,           KC_PGUP,
      KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B, KC_N, KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,      KC_RSFT,                  KC_UP,   KC_PGDN,
-     KC_LCTL,  KC_LGUI,  KC_LALT,                            LT(FN2,KC_SPC)                   KC_RALT,   MO(WIN_FN1), MO(FN2), KC_LEFT, KC_DOWN, KC_RGHT),
+     KC_LCTL,  KC_LGUI,  KC_LALT,                            LT(FN2,KC_SPC),                   KC_RALT,   MO(WIN_FN1), MO(FN2), KC_LEFT, KC_DOWN, KC_RGHT),
 
 [MAC_FN1] = LAYOUT_ansi_68(
      KC_GRV,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,            _______,
